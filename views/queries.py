1d6f546ba85b2f5d5239ce888ec61c90d119bd41
matt.carr <matt.carr@purplesecure.com>
2016-03-18 09:36:16 +0000

    refactored methods

--- views/queries.py
+++ views/queries.py
@@ -4,6 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 from adapters.certuk_adhoc.query.address import get_addresses
 from adapters.certuk_adhoc.query.domain_names import get_domain_names
+from adapters.certuk_adhoc.query.file_hashes import get_file_hashes
 from adapters.certuk_adhoc.common.logger import log_error
 from adapters.certuk_adhoc.query.cleanse_data import cleanse_data_list
 
@@ -12,6 +13,22 @@ from django.http.response import HttpResponse, JsonResponse
 
 
 REGEX_ADDRESS_DELIMITER = re.compile("[\n,]")
+
+
+def generateResponse(matches_cursor, request, elapsed):
+        matches = []
+        for match_result in matches_cursor:
+            match_result['_id']=match_result['objects']
+        if request.META.get('HTTP_ACCEPT') in {'application/json', 'text/json'}:
+            return JsonResponse({
+                'duration': '%.2f' % elapsed.ms(),
+                'matches': matches,
+                'state': 'success'
+            }, status=200)
+        else:
+            plain_text = "\n".join("%s - %s" % (address, ", ".join(ids)) for address, ids in matches.iteritems()) + "\n"
+            return HttpResponse(content=plain_text, content_type='text/plain')
+
 
 
 @csrf_exempt
@@ -25,19 +42,7 @@ def address(request):
         raw_body = request.body
         addresses = REGEX_ADDRESS_DELIMITER.split(raw_body)
         matches_cursor = get_addresses(cleanse_data_list(addresses))
-        matches = {}
-        for match_result in matches_cursor:
-            address = match_result['_id']
-            matches[address] = match_result['objects']
-        if request.META.get('HTTP_ACCEPT') in {'application/json', 'text/json'}:
-            return JsonResponse({
-                'duration': '%.2f' % elapsed.ms(),
-                'matches': matches,
-                'state': 'success'
-            }, status=200)
-        else:
-            plain_text = "\n".join("%s - %s" % (address, ", ".join(ids)) for address, ids in matches.iteritems()) + "\n"
-            return HttpResponse(content=plain_text, content_type='text/plain')
+        generateResponse(matches_cursor, request, elapsed)
     except Exception as e:
         log_error(e, 'adapters/address_search', 'Bulk address search failed')
         return JsonResponse({
@@ -70,6 +75,46 @@ def domain_names(request):
         }, status = 200)
     except Exception as e:
         log_error(e, 'adapter/domain_names', 'Domain name search failed')
+        return JsonResponse({
+            'duration': '%.2f' %elapsed.ms(),
+            'message': e.message,
+            'state': 'error'
+        }, status = 500)
+
+
+
+@csrf_exempt
+def file_hashes(request):
+
+    if not request.method == 'POST':
+        return JsonResponse({'Request must be': 'POST'}, status=405)
+
+    elapsed = StopWatch()
+    try:
+        raw_body = request.body
+        hashes = REGEX_ADDRESS_DELIMITER.split(raw_body)
+        hashes_raw = cleanse_data_list(hashes)
+        hashes_upper = [x.upper() for x in hashes_raw]
+        matches_cursor = get_file_hashes(hashes_upper)
+        matches = []
+        for match_result in matches_cursor:
+            print match_result
+        return JsonResponse({
+            'duration': '$.2sf' %elapsed.ms(),
+            'matches': matches,
+            'state': 'success'
+        })
+    except Exception as e:
+        log_error(e, 'adapter/file_hashes', 'File hash search failed')
+        return JsonResponse({
+            'duration': '%.2f' %elapsed.ms(),
+            'message': e.message,
+            'state': 'error'
+        }, status = 500)
+
+
+def generateErrorResponse(method_name, elapsed, e):
+        log_error(e, 'adapter' + method_name, method_name + ' search failed')
         return JsonResponse({
             'duration': '%.2f' %elapsed.ms(),
             'message': e.message,

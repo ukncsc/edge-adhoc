2607f7aad93cb9d62ae5ee92ffef8eb1974afdf1
matt.carr <matt.carr@purplesecure.com>
2016-04-12 09:09:22 +0100

    ignore last 2 commits, changes made from ADHOC#8

--- query/file_hashes.py
+++ query/file_hashes.py
@@ -1,6 +1,6 @@
 from mongoengine.connection import get_db
 
-def get_file_hashes_xsi(data):
+def get_file_hashes(data):
     if not data:
         raise Exception("No file hashes supplied")
     matches_cursor = get_db().stix.aggregate([
@@ -21,41 +21,6 @@ def get_file_hashes_xsi(data):
         {
             '$group': {
                 '_id':'$data.api.object.properties.hashes.type',
-                'objects': {
-                    '$push': '$_id'
-                }
-            }
-        },
-        {
-            '$sort': {'_id':1}
-        }
-    ], cursor={})
-
-    return matches_cursor
-
-
-def get_file_hashes_no_xsi(data):
-    if not data:
-        raise Exception("No file hashes supplied")
-    matches_cursor = get_db().stix.aggregate([
-        {
-            '$match': {
-                'data.summary.type': 'FileObjectType'
-            }
-        },
-        {
-            '$unwind': '$data.api.object.properties.hashes'
-        },
-        {
-            '$match': {
-                'data.api.object.properties.hashes.type.value': {
-                    '$in': data
-            }
-            }
-        },
-        {
-            '$group': {
-                '_id':'$data.api.object.properties.hashes.type.value',
                 'objects': {
                     '$push': '$_id'
                 }
